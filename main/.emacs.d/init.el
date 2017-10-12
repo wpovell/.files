ecabf6b70e8aef1f4b1cf72d6df1810a41656a0e
Will Povell <william_povell@brown.edu>
2017-10-11 20:00:02 -0400

    Merged bin, emacs stuff

---
+++ main/.emacs.d/init.el
@@ -0,0 +1,50 @@
+(setq backup-directory-alist `(("." . "~/.saves")))
+(setq linum-format "%d  ")
+
+(global-linum-mode t)
+(setq inhibit-startup-message t) ;; hide the startup message
+
+(require 'package)
+
+(add-to-list 'package-archives
+       '("melpa" . "http://melpa.org/packages/") t)
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar myPackages
+  '(better-defaults
+    flycheck
+    py-autopep8
+    elpy))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+(elpy-enable)
+(setq elpy-rpc-python-command "python3")
+(setq python-shell-interpreter "python3")
+
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-tooltip ((t (:foreground "white")))))
--- main/.zshrc
+++ main/.zshrc
@@ -1,29 +1,18 @@
-# Cuda Stuff
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"
 export CUDA_HOME=/usr/local/cuda
 
 # Path
 export PATH=$PATH:~/bin/:~/dotfiles/bin:/sbin/:/usr/local/cuda-8.0/bin:~/.local/bin
-
 # Reload Xresources
 alias xup="xrdb -I$HOME ~/.Xresources"
 # Never open emacs in a window
 alias emacs="emacs -nw"
-# Ls, now with color!
-alias ls='ls --color=auto'
-alias lsa='ls -a'
-# Max num of dirs
-PROMPT_DIRTRIM=2
+alias ls="ls --color"
 
-# Prompt
+local host_name="%{$fg[cyan]%}blueno"
+local path_string="%{$fg[yellow]%}%~"
+local prompt_string="»"
+
 C1="\[\033[34m\]"
 END="\[\033[0m\]"
-PS1="$C1\h$END \w $C1»$END "
-
-# Ignore repeat history
-HISTCONTROL=ignoreboth
-# Size
-HISTSIZE=500
-
-# Disable C-s
-stty -ixon
+PROMPT="%{%F{blue}%}%M%{%f%} %4(c:.../:)%3c %{%F{blue}%}»%{%f%} "
