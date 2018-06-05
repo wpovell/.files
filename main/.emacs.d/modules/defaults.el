2c0a47d8fae0cc3786dc5eeeb3b892caf3640bfb
Will Povell <william_povell@brown.edu>
2018-06-04 20:49:16 -0700

    Added window movement

--- main/.emacs.d/modules/defaults.el
+++ main/.emacs.d/modules/defaults.el
@@ -1,6 +1,12 @@
 ;; Fish some ansi-term newline issues
 (setq term-suppress-hard-newline t)
 
+;; Window movement
+(global-set-key (kbd "<M-right>") 'windmove-right)
+(global-set-key (kbd "<M-left>") 'windmove-left)
+(global-set-key (kbd "<M-up>") 'windmove-up)
+(global-set-key (kbd "<M-down>") 'windmove-down)
+
 ;; Ido
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
