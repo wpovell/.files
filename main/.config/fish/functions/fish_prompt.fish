89b6ea0e3c647d719868ecc34050393c0d3659bc
William Povell <wpovell@stripe.com>
2018-06-14 13:22:40 -0700

    Added better git prompt to fish

--- main/.config/fish/functions/fish_prompt.fish
+++ main/.config/fish/functions/fish_prompt.fish
@@ -7,8 +7,16 @@ function fish_prompt
   set_color blue
   set branch (git branch 2>/dev/null | grep "*" | cut -f2 -d" ")
   if [ "$branch" != "" ]
-     echo -n " [$branch] "
+     git diff --no-ext-diff --quiet --exit-code
+     if [ $status != 0 ]
+         set_color yellow
+     end
+     echo -n "  "
+     if [ "$branch" != "master" ]
+         echo -n "[$branch] "
+     end
   end
+  set_color blue
   echo -en "\n» "
   set_color normal
 end
