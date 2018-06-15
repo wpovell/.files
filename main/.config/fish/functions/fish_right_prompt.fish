faa153aac495a26683489e5d6adb093f9652a506
William Povell <wpovell@stripe.com>
2018-06-15 11:17:24 -0700

    Better right prompt

--- main/.config/fish/functions/fish_right_prompt.fish
+++ main/.config/fish/functions/fish_right_prompt.fish
@@ -1,3 +1,17 @@
 function fish_right_prompt
-    echo -sn (set_color 555) (date "+%I:%M") (set_color normal)
+    set -l status_copy $status
+    set -l status_color 0fc
+
+    if test "$status_copy" -ne 0
+        set status_color $fish_color_error
+    end
+
+    if test -n "$CMD_DURATION" -a "$CMD_DURATION" -gt 100
+        set -l duration_copy $CMD_DURATION
+        set -l duration (echo $CMD_DURATION | humanize_duration)
+
+        echo -sn (set_color $status_color) "$duration" (set_color normal)
+    else
+        echo -sn (set_color 555) (date "+%I:%M") (set_color normal)
+    end
 end
