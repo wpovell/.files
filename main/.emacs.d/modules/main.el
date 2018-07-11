8d82483ea07af21ad51f6e98469aacaa05b24cc9
Will Povell <william_povell@brown.edu>
2018-07-10 22:12:26 -0700

    Emacs scroll

--- main/.emacs.d/modules/main.el
+++ main/.emacs.d/modules/main.el
@@ -1,5 +1,7 @@
 ;; Enable mouse in term emacs
-(xterm-mouse-mode 1)
+(xterm-mouse-mode t)
+(global-set-key (kbd "<mouse-5>") '(lambda () (interactive) (scroll-up 1)))
+(global-set-key (kbd "<mouse-4>") '(lambda () (interactive) (scroll-down 1)))
 
 ;; Keep buffers consistent with disk
 (global-auto-revert-mode t)
