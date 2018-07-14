38c49ad2c8241bd1f41f8ca13ebe25029b1dfcd7
Will Povell <william_povell@brown.edu>
2018-07-14 13:46:47 -0700

    Improved modeline to show projectile project

--- main/.emacs.d/modules/modeline.el
+++ main/.emacs.d/modules/modeline.el
@@ -6,10 +6,15 @@
   (let* ((available-width (- (window-width) (length left) 2)))
     (format (format " %%s %%%ds " available-width) left right)))
 
+
 (setq-default mode-line-format
       '((:eval (simple-mode-line-render
                 (format-mode-line
-                 '("%b"
+                 '((:eval (if (projectile-project-p)
+                              (concat
+                               (projectile-project-name)
+                               " | ")))
+                   "%b"
                    (:eval (if (and (buffer-modified-p) (not buffer-read-only)) "*" ""))
                    (:eval (if buffer-read-only " " " (%l:%C)"))))
                 (format-mode-line
