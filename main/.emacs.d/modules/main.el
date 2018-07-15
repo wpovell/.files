d78d8575fb7b4a8a9bb3b3b09a1b42c5eba93d5d
Will Povell <william_povell@brown.edu>
2018-07-15 10:50:35 -0700

    Made flyspell Mac compatible

--- main/.emacs.d/modules/main.el
+++ main/.emacs.d/modules/main.el
@@ -71,7 +71,9 @@
 
 ;; Flyspell
 (setq flyspell-issue-welcome-flag nil)
-(setq-default ispell-program-name "/sbin/aspell")
+(if (eq system-type 'darwin)
+    (setq-default ispell-program-name "/usr/local/bin/aspell")
+  (setq-default ispell-program-name "/sbin/aspell"))
 
 ;; Highlight parens
 (show-paren-mode t)
