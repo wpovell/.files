dade9d69f8f3d98ac89202fbf91bee6d0868cbae
Will Povell <william_povell@brown.edu>
2018-07-15 09:32:59 -0700

    Added modeline flash for ding

--- main/.emacs.d/modules/modeline.el
+++ main/.emacs.d/modules/modeline.el
@@ -27,3 +27,12 @@
                       "" vc-mode))
                     "] "))
                    mode-name))))))
+
+;; Flash modeline for ding
+(setq ring-bell-function
+      (lambda ()
+        (let ((orig-fg (face-foreground 'mode-line)))
+          (set-face-foreground 'mode-line "#F2804F")
+          (run-with-idle-timer 0.1 nil
+                               (lambda (fg) (set-face-foreground 'mode-line fg))
+                               orig-fg))))
