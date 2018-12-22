1e1930481e89319117fa2d4d43e3fdf6d8b3bd17
Will Povell <william_povell@brown.edu>
2018-12-22 01:05:41 -0500

    Deleted fish config comments bc it broke stuff

--- main/.config/fish/config.fish
+++ main/.config/fish/config.fish
@@ -3,14 +3,7 @@ function setenv
         # This is a big hack to allow me to call setenv on
         # PATH multiple times without messing up my bash config
         # TODO: Make this better
-        set -gx PATH (
-        echo $argv[2..-1] | \ # Get path args
-        tr ': ' \n | \        # Trim colons and spaces
-        nl | \                # Number lines
-        sort -k 2| \          # Sort path entries
-        uniq -f 1 | \         # Get unique path entries
-        sort -n| \            # Sort by line numbers
-        cut -f 2)             # Remove line numbers
+        set -gx PATH (echo $argv[2..-1] | tr ': ' \n | nl | sort -k 2 | uniq -f 1 | sort -n | cut -f 2)
     else
         set -gx $argv
     end
