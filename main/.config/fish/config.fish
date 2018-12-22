37dade94d9ced33d10ad8f9bf76a96b88b229d11
Will Povell <william_povell@brown.edu>
2018-12-22 00:58:53 -0500

    Added stty to fish config
    Added stty call to hopefully get rid of weird alacritty errors about
    no row/col set. Also commented my horrible setenv PATH hack.

--- main/.config/fish/config.fish
+++ main/.config/fish/config.fish
@@ -1,11 +1,24 @@
 function setenv
     if [ $argv[1] = PATH ]
-        # Replace colons and spaces with newlines
-        set -gx PATH (echo $argv[2..-1] | tr ': ' \n | nl | sort -k 2| uniq -f 1 | sort -n| cut -f 2)
+        # This is a big hack to allow me to call setenv on
+        # PATH multiple times without messing up my bash config
+        # TODO: Make this better
+        set -gx PATH (
+        echo $argv[2..-1] | \ # Get path args
+        tr ': ' \n | \        # Trim colons and spaces
+        nl | \                # Number lines
+        sort -k 2| \          # Sort path entries
+        uniq -f 1 | \         # Get unique path entries
+        sort -n| \            # Sort by line numbers
+        cut -f 2)             # Remove line numbers
     else
         set -gx $argv
     end
 end
+
+# Added to supress warnings about
+# unset rows/cols in alacritty
+stty >/dev/null 2>&1
 
 source ~/.env
 source ~/.aliases
