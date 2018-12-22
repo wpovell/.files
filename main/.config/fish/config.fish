55e0952dc004c1a34e23f2980af8d87c86480525
Will Povell <william_povell@brown.edu>
2018-12-22 12:52:43 -0500

    Removed stty from fish config
    Added to try and fix issues with creating terminals from full screen
    alacritty but didn't work

--- main/.config/fish/config.fish
+++ main/.config/fish/config.fish
@@ -2,16 +2,16 @@ function setenv
     if [ $argv[1] = PATH ]
         # This is a big hack to allow me to call setenv on
         # PATH multiple times without messing up my bash config
+        # Essentially turns into space seperated list of values
+        # and then removes duplicates that somehow magically
+        # show up.
+
         # TODO: Make this better
         set -gx PATH (echo $argv[2..-1] | tr ': ' \n | nl | sort -k 2 | uniq -f 1 | sort -n | cut -f 2)
     else
         set -gx $argv
     end
 end
-
-# Added to supress warnings about
-# unset rows/cols in alacritty
-stty >/dev/null 2>&1
 
 source ~/.env
 source ~/.aliases
