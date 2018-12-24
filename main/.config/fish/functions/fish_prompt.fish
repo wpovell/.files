e7d8db2b45a7536a094dd976db23c0178ba163cd
Will Povell <william_povell@brown.edu>
2018-12-24 15:03:19 -0500

    Made fish prompt better

--- main/.config/fish/functions/fish_prompt.fish
+++ main/.config/fish/functions/fish_prompt.fish
@@ -1,21 +1,39 @@
 function fish_prompt
-  set_color blue
-  echo -n (hostname)" "
-  set_color normal
-  echo -n (prompt_pwd)
-  set_color blue
-  set branch (git branch 2>/dev/null | grep "*" | cut -f2 -d" ")
-  if [ "$branch" != "" ]
-     git diff --no-ext-diff --quiet --exit-code
-     if [ $status != 0 ]
-         set_color yellow
-     end
-     echo -n "  "
-     if [ "$branch" != "master" ]
-         echo -n "[$branch] "
-     end
-  end
-  set_color blue
-  echo -en "\n» "
-  set_color normal
+    # Store status of previous command
+    set failed false
+    if [ $status -ne 0 ]
+        set failed true
+    end
+
+    # Host
+    set_color blue
+    echo -n (hostname)" "
+    set_color normal
+
+    # Current dir
+    echo -n (prompt_pwd)
+    set_color blue
+
+    # Git
+    set branch (git branch 2>/dev/null | grep "*" | cut -f2 -d" ")
+    if [ "$branch" != "" ]
+        git diff --no-ext-diff --quiet --exit-code
+        if [ $status != 0 ]
+            set_color yellow
+        end
+        echo -n "  "
+        if [ "$branch" != "master" ]
+            echo -n "[$branch] "
+        end
+    end
+
+    # Prompt color is red if previous command failed
+    if [ $failed = true ]
+        set_color F00
+    else
+        set_color blue
+    end
+
+    echo -en "\n» "
+    set_color normal
 end
